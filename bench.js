9e47d92779c1c10cfdfd1a9795d4567116bfa359
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-10-25 14:34:16 +0200

    Update benchmarks

--- bench.js
+++ bench.js
@@ -1,31 +1,45 @@
 'use strict'
 
 require('dotenv').config()
-const assert = require('assert').strict
 const PubSubPostgres = require('./lib/pg-pubsub')
+const util = require('util')
+
+const sleep = util.promisify(setTimeout)
 
 ;(async () => {
   const pubsub = new PubSubPostgres({
+    reconnectMaxRetries: 100000,
     db: {
       host: process.env.DB_HOST,
       port: process.env.DB_PORT,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      password: process.env.DB_PASSWORD
     }
   })
   await pubsub.connect()
 
-  let messages = 0
+  const state = { expected: 50000, actual: 0 }
+
   pubsub.on('test', (_payload) => {
-    messages++
+    state.actual++
   })
 
-  console.time('start')
-  const count = 10000
-  for (let i = 0; i < count; i++) {
+  console.time('bench')
+  for (let i = 0; i < state.expected; i++) {
     await pubsub.emit({ topic: 'test', payload: 'payload' })
+    if (i % 1000 === 0) {
+      console.log('emit counter: ', i)
+    }
   }
-  console.timeEnd('start')
-  assert.equal(messages, count)
+  console.timeEnd('bench')
+
+  while (true) {
+    console.log('processed messages: ', state.actual)
+    if (state.actual === state.expected) {
+      break
+    }
+    await sleep(1000)
+  }
+
   await pubsub.close()
 })()
