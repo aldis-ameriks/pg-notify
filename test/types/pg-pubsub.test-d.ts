45e1d2a05b8bf8592d6ac39cbfe5fd941198db6d
Aldis Ameriks <aldis.ameriks@gmail.com>
2020-10-25 14:41:08 +0200

    Add ts types

---
+++ test/types/pg-pubsub.test-d.ts
@@ -0,0 +1,33 @@
+/* eslint-disable */
+import { expectError } from 'tsd'
+import PGPubSub from '../..'
+
+new PGPubSub({
+  continuousEmitFailureThreshold: 1,
+  emitThrottleDelay: 1,
+  maxEmitRetries: 1,
+  maxPayloadSize: 1,
+  reconnectDelay: 1,
+  reconnectMaxRetries: 1,
+  db: {
+    connectionString: 'connection string'
+  }
+})
+
+new PGPubSub({
+  continuousEmitFailureThreshold: 1,
+  emitThrottleDelay: 1,
+  maxEmitRetries: 1,
+  maxPayloadSize: 1,
+  reconnectDelay: 1,
+  reconnectMaxRetries: 1,
+  db: {
+    host: 'host',
+    password: 'password',
+    port: 5432,
+    user: 'user'
+  }
+})
+
+// db option is mandatory
+expectError(new PGPubSub({}))
